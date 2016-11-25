ca91061a37ec9d2451710dd85e6da7fda27b8606
Your Name <you@example.com>
2016-11-25 17:17:42 +0300

    test works

--- src/test/java/GreetingControllerTest.java
+++ src/test/java/GreetingControllerTest.java
@@ -1,5 +1,6 @@
-package hello;
 import hello.Application;
+import hello.Greeting;
+import hello.GreetingController;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -9,6 +10,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 import static org.junit.Assert.*;
 
 /**
@@ -20,6 +25,17 @@ public class GreetingControllerTest {
     @Test
     public void disp() throws Exception {
         System.out.println("hui");
+    }
+
+    @Test
+    public void constr() throws IOException {
+        RandomAccessFile raf = new RandomAccessFile("storage.txt", "r");
+        raf.seek(0);
+        long fromFile = raf.readLong();
+//        System.out.println(raf.readLong());
+        GreetingController gc = new GreetingController();
+
+        assertEquals(new Greeting(fromFile, "Hello, a!"), gc.display("a"));
     }
 
 }
